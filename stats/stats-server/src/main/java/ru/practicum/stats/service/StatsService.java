34fc0eade8477738960904cf0136ebbea311ff82
Oleg Terentev <terentjev.dr@yandex.ru>
2023-02-28 22:49:54 +0300

    Edit mistake

--- stats/stats-server/src/main/java/ru/practicum/stats/service/StatsService.java
+++ stats/stats-server/src/main/java/ru/practicum/stats/service/StatsService.java
@@ -7,6 +7,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public interface StatsService {
+
     void save(StatsDtoIn statsDtoIn);
     List<StatsDtoOut> get(LocalDateTime start, LocalDateTime end, List<String> uris, boolean unique);
 }
